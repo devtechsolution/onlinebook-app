a8f1c676bda1f1ff22b4a054ae6b205f70180692
devtechsolution <srivastvadevzone@gmail.com>
2019-01-12 20:33:53 +0530

    Initial commit

---
+++ src/main/java/org/devtech/onlinebookapp/OnlinebookAppApplication.java
@@ -0,0 +1,14 @@
+package org.devtech.onlinebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookAppApplication.class, args);
+	}
+
+}
+
