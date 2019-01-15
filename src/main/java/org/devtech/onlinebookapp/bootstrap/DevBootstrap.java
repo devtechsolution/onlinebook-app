280962e9705920901d5171f8967487f78019e6ad
devtechsolution <srivastvadevzone@gmail.com>
2019-01-15 10:51:49 +0530

    Added Publisher model class

--- src/main/java/org/devtech/onlinebookapp/bootstrap/DevBootstrap.java
+++ src/main/java/org/devtech/onlinebookapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package org.devtech.onlinebookapp.bootstrap;
 
 import org.devtech.onlinebookapp.model.Author;
 import org.devtech.onlinebookapp.model.Book;
+import org.devtech.onlinebookapp.model.Publisher;
 import org.devtech.onlinebookapp.repositories.AuthorRepository;
 import org.devtech.onlinebookapp.repositories.BookRepositories;
+import org.devtech.onlinebookapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,13 +15,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepositories bookRepositories;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepositories bookRepositories) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepositories bookRepositories, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepositories = bookRepositories;
+        this.publisherRepository = publisherRepository;
     }
-
-
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -28,9 +30,15 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
+        Publisher publisher= new Publisher();
+        publisher.setName("Nalanda Press");
+        publisher.setAddress("Nalanda, Bihar");
+        publisherRepository
+                .save(publisher);
+
         //TechSolution
         Author techSolution= new Author("Tech", "Solution");
-        Book java= new Book("Introduction Of Java 8", "1234", "Indian");
+        Book java= new Book("Introduction Of Java 8", "1234", publisher);
         techSolution.getBooks().add(java);
         java.getAuthors().add(techSolution);
 
@@ -39,7 +47,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //IndianPress
         Author indianpress= new Author("James", "Gosling");
-        Book spring= new Book("Introduction Of Spring", "9874", "Indian");
+        Book spring= new Book("Introduction Of Spring", "9874", publisher);
         techSolution.getBooks().add(spring);
         java.getAuthors().add(indianpress);
 
